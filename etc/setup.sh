4e2e63d01882e4bf129bd5cb34af6b52e6faf4cf
Steve Streza <stevestreza@gmail.com>
2009-03-15 15:56:08 -0400

    Moved bash completion into home dir

--- etc/setup.sh
+++ etc/setup.sh
@@ -2,20 +2,22 @@
 
 SCRIPT_DIR="etc";
 NEEDS_RESET="false";
+SETUP_DEBUG="false";
 
-#if [ "$(pwd | sed "s/^.*\/\(.*\)$/\1/g")" -eq "$SCRIPT_DIR" ]; then
-#    cd ..;
-#    NEEDS_RESET="true"
-#fi
+if [ "$(pwd | sed "s/^.*\/\(.*\)$/\1/g")" == "$SCRIPT_DIR" ]; then
+    echo "CDing down";
+    cd ..;
+    NEEDS_RESET="true"
+fi
 
 function deployFile {
     OLDFILE="$(pwd)/$1"
     NEWFILE="$HOME/$2"
     
     if [ -f $OLDFILE ]; then
-#        echo "From $OLDFILE to $NEWFILE";
+        echo "From $OLDFILE to $NEWFILE";
         if [ -f $NEWFILE ]; then
-#            echo "Backing up $NEWFILE to $NEWFILE.bak"
+            echo "Backing up $NEWFILE to $NEWFILE.bak"
             mv $NEWFILE $NEWFILE.bak
         fi
         ln -s $OLDFILE $NEWFILE;
@@ -27,9 +29,11 @@ deployFile bash_common.private .bash_common.private
 deployFile bash_profile .bash_profile
 deployFile bashrc .bashrc
 deployFile gitignore .gitignore
-deployFile screenrc .bash_common
+deployFile screenrc .screenrc
+deployFile etc/bash_completion .bash_completion
 deployFile etc/git-completion.bash .git-completion.bash
 
-#if [ "$NEEDS_RESET" -eq "true" ]; then
-#    cd $SCRIPT_DIR;
-#fi;
+if [ "$NEEDS_RESET" == "true" ]; then
+    echo "CDing up to $SCRIPT_DIR";
+    cd $SCRIPT_DIR;
+fi;
